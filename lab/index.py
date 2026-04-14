43f1a4810e66eca733637f83f52e61e81f3f8061
LaiKhanh <125262089+LaiKhanh@users.noreply.github.com>
2026-04-14 14:48:03 +0700

    Delete comment code in index.py

--- lab/index.py
+++ lab/index.py
@@ -1,17 +1,3 @@
-# import chromadb, os
-# from sentence_transformers import SentenceTransformer
-
-# client = chromadb.PersistentClient(path='./chroma_db')
-# col = client.get_or_create_collection('day09_docs')
-# model = SentenceTransformer('all-MiniLM-L6-v2')
-
-# docs_dir = './data/docs'
-# for fname in os.listdir(docs_dir):
-#     with open(os.path.join(docs_dir, fname), encoding='utf-8') as f:
-#         content = f.read()
-#     print(f'Indexed: {fname}')
-# print('Index ready.')
-
 import chromadb, os
 from sentence_transformers import SentenceTransformer
 
