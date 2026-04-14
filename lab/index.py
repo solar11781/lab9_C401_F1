1e414d3e04f301e6453a0024b8ac87e321316cff
LaiKhanh <125262089+LaiKhanh@users.noreply.github.com>
2026-04-14 14:22:09 +0700

    build index

---
+++ lab/index.py
@@ -0,0 +1,13 @@
+import chromadb, os
+from sentence_transformers import SentenceTransformer
+
+client = chromadb.PersistentClient(path='./chroma_db')
+col = client.get_or_create_collection('day09_docs')
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+docs_dir = './data/docs'
+for fname in os.listdir(docs_dir):
+    with open(os.path.join(docs_dir, fname), encoding='utf-8') as f:
+        content = f.read()
+    print(f'Indexed: {fname}')
+print('Index ready.')
